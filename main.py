cda44398bf804dbc4cadbdee9cc847a7700117cf
nadebu <nadebu@gmail.com>
2024-07-05 11:43:49 +0100

    focus on events

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from event_manager import EventManager
-from pprint import pprint
 
 event_manager = EventManager()
 
-pprint(event_manager.get_attendee_details())
+print(event_manager.get_org_events())
